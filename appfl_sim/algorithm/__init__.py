b70669a5d9e15f8a8425d0669564b33b519718e4
Adam Hahn <vaseline555@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2026-03-28 00:23:12 +0000

    latest

--- appfl_sim/algorithm/__init__.py
+++
@@ -1,5 +0,0 @@
-"""APPFL algorithm namespace for simulation."""
-
-from appfl_sim.algorithm import aggregator, scheduler, trainer
-
-__all__ = ["aggregator", "scheduler", "trainer"]
--- appfl_sim/algorithm/trainer/swucb_trainer.py
+++
@@ -1,5 +0,0 @@
-from appfl_sim.algorithm.trainer.dsucb_trainer import DsucbTrainer
-
-
-class SwucbTrainer(DsucbTrainer):
-    pass
