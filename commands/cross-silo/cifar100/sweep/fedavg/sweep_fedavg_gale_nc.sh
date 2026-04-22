738b8d9740cf1d8c5d880556b7f0627f867a97bb
vaseline555 <hahns@anl.gov>
2026-04-22 03:56:08 +0000

    updated

--- commands/cross-silo/cifar100/sweep/fedavg/sweep_fedavg_gale_nc.sh
+++ commands/cross-silo/cifar100/sweep/fedavg/sweep_fedavg_gale_nc.sh
@@ -1,5 +1,28 @@
 #!/bin/bash -l
 
+#PBS -N GALE
+#PBS -A PPFL_FM
+#PBS -q preemptable
+#PBS -l select=1:system=polaris
+#PBS -l place=scatter
+#PBS -l walltime=18:00:00
+#PBS -l filesystems=home:eagle
+#PBS -r y
+#PBS -k doe
+#PBS -j oe
+
+set -euo pipefail
+
+cd "${PBS_O_WORKDIR:-$PWD}"
+
+module use /soft/modulefiles
+module load conda
+conda activate base
+
+export http_proxy="http://proxy.alcf.anl.gov:3128"
+export https_proxy="http://proxy.alcf.anl.gov:3128"
+export ftp_proxy="http://proxy.alcf.anl.gov:3128"
+
 WANDB_ENTITY="${WANDB_ENTITY:-vaseline555}"
 WANDB_MODE="${WANDB_MODE:-online}"
 WANDB_PROJECT="${WANDB_PROJECT:-GALE_SWEEP}"
